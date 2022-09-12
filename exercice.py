7c6084e2e89867f405b495f0d637ed6cf5a199dc
Abdul Chaarani <75145721+abdulchaarani@users.noreply.github.com>
2022-09-11 20:53:41 -0400

    exercice termine

--- exercice.py
+++ exercice.py
@@ -2,33 +2,44 @@
 
 
 def dissipated_power(voltage, resistance):
-	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	# P = V² / R
+	return (voltage**2)/resistance
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+	# v1[0] # Pour accéder au X
+	# v1[1] # Pour accéder au Y
+	produit_scalaire = v1[0] * v2[0] + v1[1] * v2[1]
+	return produit_scalaire == 0
 
 def average(values):
-	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	valeurs_positives = 0
+	somme = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v >= 0:
+			somme += v
+			valeurs_positives += 1
+	if valeurs_positives:
+		return somme/valeurs_positives
+	else: return 0
 
 def bills(value):
 	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
 	while value != 0:
 		if value >= 20:
-			pass
+			twenties = value // 20
+			value %= 20
 		elif value >= 10:
-			pass
+			tens = value // 10
+			value %= 10
 		elif value >= 5:
-			pass
+			fives = value // 5
+			value %= 5
 		elif value >= 1:
-			pass
+			ones = value // 1
+			value %= 1
 
-	return (twenties, tens, fives, twos, ones);
+	return (twenties, tens, fives, ones);
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
@@ -36,11 +47,11 @@ def format_base(value, base, digit_letters):
 	result = ""
 	abs_value = abs(value)
 	while abs_value != 0:
-		pass
+		result += digit_letters[abs_value % base]
+		abs_value = abs_value // base
 	if value < 0:
-		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
-		pass
-	return result
+		result += '-'
+	return result[::-1]
 
 
 if __name__ == "__main__":
